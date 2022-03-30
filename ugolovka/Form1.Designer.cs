1378df77fc763e2dade80add119be97224b014a0
tfenist <puzyryov2000@mail.ru>
2022-03-30 16:15:31 +0300

    Создание проекта, формы, загрузка и сохранение текстовых файлов, перевод текста в нижний регистр.

--- ugolovka/Form1.Designer.cs
+++ ugolovka/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(571, 345);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(604, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(571, 345);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Загрузить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(604, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 75);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сохранить файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1187, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
     }
 }
--- ugolovka/Form1.cs
+++ ugolovka/Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace ugolovka
 {
     public partial class Form1 : Form
@@ -5,6 +10,51 @@ namespace ugolovka
         public Form1()
         {
             InitializeComponent();
+
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*"; // Фильтр для типов файлов
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        public string fileText = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            StreamReader sr = new StreamReader(openFileDialog1.FileName, Encoding.UTF8); // Считывание текста из файла в кодировке utf-8
+
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+
+            // читаем файл по символьно и записываем текст в обну строку
+            fileText += sr.ReadToEnd();
+            fileText = fileText.Replace("\r\n", " ");
+
+            string pattern = @"\w+\s*"; // Задаем паттерн
+            Regex regex = new Regex(pattern);
+
+            string result = "";
+
+            foreach (Match match in regex.Matches(fileText)) // Оставляем в тексте алфавитно-цифровые и пробельные символы 
+            {
+                result += match.Value;
+            }
+
+            richTextBox1.Text = result.ToLowerInvariant(); // Переведим текст в нижний регистр
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            File.WriteAllText(saveFileDialog1.FileName, richTextBox2.Text);
+            MessageBox.Show("Фаил сохранен!");
         }
     }
 }
